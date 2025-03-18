01a06c8a3273ec8bf3e60c8c7ce97dd15f8dcde6
Roman Soltan <romabandit88@gmail.com>
2025-03-18 07:11:12 +0200

    finally

--- module_1/lesson_2/src/index.js
+++ module_1/lesson_2/src/index.js
@@ -1,2 +1,12 @@
-const message = "Hello from Node.js ";
+const message = 'Hello from Node.js ';
 console.log(message);
+
+const student = {
+  id: 123,
+  name: 'John Doe',
+  age: 16,
+  gender: 'male',
+  onDuty: false,
+};
+
+console.log(student);
