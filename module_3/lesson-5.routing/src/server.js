0f165283a1ffcff1e3845ec30adb4027a6f3b039
Roman Soltan <romabandit88@gmail.com>
2025-03-30 23:07:09 +0300

    notFoundHandler, errorHandler

--- module_3/lesson-5.routing/src/server.js
+++ module_3/lesson-5.routing/src/server.js
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 import studentsRouter from './routers/students.js';
 
 import { getEnvVar } from './utils/getEnvVar.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 dotenv.config();
 
@@ -38,19 +40,11 @@ export const startServer = () => {
   // Додаємо роутер до app як middleware
   app.use(studentsRouter);
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  // обробка неіснуючого маршруту
+  app.use('*', notFoundHandler);
 
   // Middleware для обробки помилок (приймає 4 аргументи)
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
