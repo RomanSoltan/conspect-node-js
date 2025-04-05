16e9dc478d3b018948386b5060cf037fba068f9b
Roman Soltan <romabandit88@gmail.com>
2025-04-05 22:04:04 +0300

    init

---
+++ module_4/lesson_7.Validation_Joi/src/routers/students.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import {
+  createStudentController,
+  deleteStudentController,
+  getStudentByIdController,
+  getStudentsController,
+  patchStudentController,
+  upsertStudentController,
+} from '../controllers/students.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+// Маршрут для обробки GET-запитів на '/students'
+router.get('/students', ctrlWrapper(getStudentsController));
+
+// Маршрут для обробки GET-запитів на '/students:studentId'
+router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
+
+router.post('/students', ctrlWrapper(createStudentController));
+
+router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
+
+router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
+
+router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+
+export default router;
---
+++ module_4/lesson_7.Validation_Joi/src/server.js
@@ -0,0 +1,57 @@
+import express from 'express';
+// import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+// Імпортуємо роутер
+import studentsRouter from './routers/students.js';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+dotenv.config();
+
+// Читаємо змінну оточення PORT
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  // Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+  // наприклад, у запитах POST або PATCH
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
+  app.use(cors());
+
+  // app.use(
+  //   pino({
+  //     transport: {
+  //       target: 'pino-pretty',
+  //     },
+  //   }),
+  // );
+
+  // Маршрут для обробки GET-запитів на '/'
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  // Додаємо роутер до app як middleware
+  app.use(studentsRouter);
+
+  // обробка неіснуючого маршруту
+  app.use('*', notFoundHandler);
+
+  // Middleware для обробки помилок (приймає 4 аргументи)
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
