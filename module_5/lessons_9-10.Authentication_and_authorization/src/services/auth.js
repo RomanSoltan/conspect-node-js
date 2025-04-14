4e3e74006ada8a91d1404bd2abe72bffb8646ee4
Roman Soltan <romabandit88@gmail.com>
2025-04-14 23:21:48 +0300

    refuctoring registerUser, hash password

--- module_5/lessons_9-10.Authentication_and_authorization/src/services/auth.js
+++ module_5/lessons_9-10.Authentication_and_authorization/src/services/auth.js
@@ -1,5 +1,19 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
 
 export const registerUser = async (payload) => {
-  return await UsersCollection.create(payload);
+  // Нам варто перевіряти email на унікальність під
+  // час реєстрації та, у разі дублювання, повертати відповідь зі статусом 409
+  // і відповідним повідомленням.
+  const user = await UsersCollection.findOne({ email: payload.email });
+  if (user) throw createHttpError(409, 'Email in use');
+
+  // ми застосуємо хешування для зберігання паролю і скористаємось бібліотекою bcrypt
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  return await UsersCollection.create({
+    ...payload,
+    password: encryptedPassword,
+  });
 };
