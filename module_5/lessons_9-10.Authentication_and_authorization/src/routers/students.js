e3315bf2a045fcddd1592b4bfdfaf318b65b9b09
Roman Soltan <romabandit88@gmail.com>
2025-04-14 22:20:08 +0300

    init

---
+++ module_5/lessons_9-10.Authentication_and_authorization/src/routers/students.js
@@ -0,0 +1,54 @@
+import { Router } from 'express';
+import {
+  createStudentController,
+  deleteStudentController,
+  getStudentByIdController,
+  getStudentsController,
+  patchStudentController,
+  upsertStudentController,
+} from '../controllers/students.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createStudentSchema,
+  updateStudentSchema,
+} from '../validation/students.js';
+import { isValidId } from '../middlewares/isValidId.js';
+
+const router = Router();
+
+router.get('/students', ctrlWrapper(getStudentsController));
+
+router.get(
+  '/students/:studentId',
+  isValidId,
+  ctrlWrapper(getStudentByIdController),
+);
+
+router.post(
+  '/students',
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
+
+router.delete(
+  '/students/:studentId',
+  isValidId,
+  ctrlWrapper(deleteStudentController),
+);
+
+router.put(
+  '/students/:studentId',
+  isValidId,
+  validateBody(createStudentSchema),
+  ctrlWrapper(upsertStudentController),
+);
+
+router.patch(
+  '/students/:studentId',
+  isValidId,
+  validateBody(updateStudentSchema),
+  ctrlWrapper(patchStudentController),
+);
+
+export default router;
