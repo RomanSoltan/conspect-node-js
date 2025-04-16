3730e56f93086771c9c52c1d8fba18494ca7755b
Roman Soltan <romabandit88@gmail.com>
2025-04-16 18:59:37 +0300

    authenticate

---
+++ module_5/lessons_9-10.Authentication_and_authorization/src/middlewares/authenticate.js
@@ -0,0 +1,51 @@
+import createHttpError from 'http-errors';
+
+import { SessionsCollection } from '../db/models/session.js';
+import { UsersCollection } from '../db/models/user.js';
+
+export const authenticate = async (req, res, next) => {
+  // Перевірка заголовка авторизації:
+  const authHeader = req.get('Authorization');
+
+  if (!authHeader) {
+    next(createHttpError(401, 'Please provide Authorization header'));
+    return;
+  }
+
+  // Перевірка типу заголовка та наявності токена:
+  const bearer = authHeader.split(' ')[0];
+  const token = authHeader.split(' ')[1];
+
+  if (bearer !== 'Bearer' || !token) {
+    next(createHttpError(401, 'Auth header should be of type Bearer'));
+    return;
+  }
+
+  // Перевірка наявності сесії:
+  const session = await SessionsCollection.findOne({ accessToken: token });
+
+  if (!session) {
+    next(createHttpError(401, 'Session not found'));
+    return;
+  }
+
+  const isAccessTokenExpired =
+    new Date() > new Date(session.accessTokenValidUntil);
+
+  if (isAccessTokenExpired) {
+    next(createHttpError(401, 'Access token expired'));
+  }
+
+  // Пошук користувача:
+  const user = await UsersCollection.findById(session.userId);
+
+  if (!user) {
+    next(createHttpError(401));
+    return;
+  }
+
+  // Додавання користувача до запиту:
+  req.user = user;
+
+  next();
+};
