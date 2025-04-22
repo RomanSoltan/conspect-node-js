83132195f3eba344db1f82559bfc6ff74c5f063a
Roman Soltan <romabandit88@gmail.com>
2025-04-22 22:41:27 +0300

    init repo

---
+++ module_6/lesson_12.Image/src/db/models/student.js
@@ -0,0 +1,41 @@
+// src/db/models/student.js
+
+import { model, Schema } from 'mongoose';
+
+const studentsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ['male', 'female', 'other'],
+    },
+    avgMark: {
+      type: Number,
+      required: true,
+    },
+    onDuty: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    parentId: {
+      // нова властивість
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const StudentsCollection = model('students', studentsSchema);
